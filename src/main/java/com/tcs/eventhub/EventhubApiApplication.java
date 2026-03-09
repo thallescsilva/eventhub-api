e2baec3e213b8bfc4d1d62219263fcf40b75cbc7
Thalles Silva <thallescezariosilva@gmail.com>
2026-03-09 19:15:00 -0300

    initial project setup with spring boot 3.5 and java 21

---
+++ src/main/java/com/tcs/eventhub/EventhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.eventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubApiApplication.class, args);
+	}
+
+}
