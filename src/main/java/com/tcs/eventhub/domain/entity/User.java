5bc1377190816fd59913f895d3a2a2e3403a5ce5
Thalles Silva <thallescezariosilva@gmail.com>
2026-03-10 19:10:00 -0300

    feat: add user entity and jwt service

---
+++ src/main/java/com/tcs/eventhub/domain/entity/User.java
@@ -0,0 +1,32 @@
+package com.tcs.eventhub.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public enum Role {
+        ADMIN, USER
+    }
+}
